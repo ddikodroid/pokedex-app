7221bd213f67533f2239ec97613549b93fb9b1c0
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-17 10:36:48 +0700

    get api of type and ability of pokemon

--- src/components/PokeDetail.js
+++ src/components/PokeDetail.js
@@ -1,7 +1,36 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View, Modal, Button, Dimensions} from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from 'react-native';
+import axios from 'axios';
 const PokeDetail = (props) => {
+  const [typePokemon, setTypePokemon] = useState([]);
+  const [abilityPokemon, setAbilityPokemon] = useState([]);
+  const uriBasic = props.uri;
+  const uriShiny = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${props.index}.png`;
+  const [imagePokemon, setImagePokemon] = useState(true);
+  const changeToShiny = () => {
+    setImagePokemon(!imagePokemon);
+  };
+  useEffect(() => {
+    axios.get(props.data).then((response) => {
+      setTypePokemon(
+        response.data.types.map((type) => type.type.name).join(', '),
+      );
+      setAbilityPokemon(
+        response.data.abilities
+          .map((ability) => ability.ability.name)
+          .join(', '),
+      );
+      console.log(abilityPokemon);
+    });
+  });
   return (
     <Modal
       animationType="slide"
@@ -10,8 +39,21 @@ const PokeDetail = (props) => {
       onRequestClose={props.onPress}>
       <View style={styles.centeredView}>
         <View style={styles.card}>
-          <Text>PokeDetail</Text>
-          <Button onPress={props.onPress} title="Keluar" />
+          <Image
+            style={styles.image}
+            resizeMode="contain"
+            source={{uri: imagePokemon ? uriBasic : uriShiny}}
+          />
+          <TouchableOpacity onPress={changeToShiny}>
+            <Text style={styles.buttonText}>
+              {imagePokemon ? 'Shiny Form' : 'Basic Form'}
+            </Text>
+          </TouchableOpacity>
+          <Text>Type(s): {typePokemon}</Text>
+          <Text>Abilitie(s): {abilityPokemon}</Text>
+          <TouchableOpacity onPress={props.onPress}>
+            <Text style={styles.buttonText}>Back</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </Modal>
@@ -24,10 +66,10 @@ const styles = StyleSheet.create({
   card: {
     alignItems: 'center',
     justifyContent: 'center',
-    height: height/1.5,
+    height: height / 1.5,
     width: width / 1.2,
     backgroundColor: 'white',
-    borderRadius: width * 0.05,
+    borderRadius: width * 0.1,
     shadowColor: 'black',
     shadowOffset: {width: 0, height: 1},
     shadowOpacity: 0.8,
@@ -37,5 +79,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
