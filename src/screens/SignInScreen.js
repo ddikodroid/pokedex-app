b46ee828ae9abd82f1b6212101e9b092eb1240e2
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-18 13:49:15 +0700

    sign in and sign up screen and styling

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,55 +1,93 @@
 import React, {useState} from 'react';
-import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
-const SignInScreen = () => {
-  const [username, setUsername] = useState([]);
-  const [password, setPassword] = useState([]);
+const SignInScreen = ({navigation}) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const NavigateToHomeScreen = () => {
+    navigation.navigate('Home', {username});
+  };
+  const NavigateToSignUpScreen = () => {
+    navigation.navigate('Sign Up');
+  };
   return (
     <View style={styles.container}>
-      <Text>Username</Text>
-      <TextInput
-        value={username}
-        onChangeText={() => setUsername(username)}
-        label="username"
-        style={styles.input}
-      />
-      <Text>Password</Text>
-      <TextInput
-        value={password}
-        onChangeText={() => setPassword(username)}
-        label="password"
-        style={styles.input}
-        secureTextEntry={true}
-      />
-      <Button title={'Login'} style={styles.input} />
+      <View style={{marginHorizontal: width * 0.05}}>
+        <Text>Welcome back,</Text>
+        <Text style={styles.title}>Sign in here👇</Text>
+        <TextInput
+          value={username}
+          onChangeText={(username) => setUsername(username)}
+          label="username"
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="#757575"
+        />
+        <TextInput
+          value={password}
+          onChangeText={(password) => setPassword(username)}
+          label="password"
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Password"
+          placeholderTextColor="#757575"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={NavigateToHomeScreen}>
+          <Text style={{fontSize: 16, fontWeight: 'bold'}}>Sign In</Text>
+        </TouchableOpacity>
+        <View style={{flexDirection: 'row', marginVertical: height * 0.01}}>
+          <Text style={{fontSize: 15}}>Don't have an account? </Text>
+          <TouchableOpacity onPress={NavigateToSignUpScreen}>
+            <Text style={{color: '#0288d1', fontSize: 15}}>Click here</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default SignInScreen;
-
+const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: width * 0.04,
   },
   input: {
-    width: 200,
-    height: 44,
+    width: width * 0.7,
+    height: height * 0.055,
     padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
+    borderWidth: 0.5,
+    borderColor: '#bdbdbd',
+    //marginBottom: 10,
+    marginVertical: height * 0.01,
+    borderRadius: width * 0.02,
   },
-  inputText: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    textAlign: 'center',
+  title: {
+    fontSize: 28,
     fontWeight: 'bold',
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
+    alignSelf: 'flex-start',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * 0.7,
+    height: height * 0.055,
+    backgroundColor: '#1de9b6',
+    borderRadius: width * 0.02,
+    marginVertical: height * 0.01,
   },
 });
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,14 +1,83 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, TextInput, TouchableOpacity } from 'react-native'
 
 const SignUpScreen = () => {
     return (
-        <View>
-            <Text>SignUp</Text>
-        </View>
+        <View style={styles.container}>
+      <View style={{marginHorizontal: width * 0.05}}>
+        <Text>Start the journey,</Text>
+        <Text style={styles.title}>Sign up here👇</Text>
+        <TextInput
+          //value={username}
+          label="firstName"
+          style={styles.input}
+          placeholder="First Name"
+          placeholderTextColor="#757575"
+        />
+                <TextInput
+          //value={username}
+          label="lastName"
+          style={styles.input}
+          placeholder="Last Name"
+          placeholderTextColor="#757575"
+        />
+        <TextInput
+          //value={username}
+          label="username"
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="#757575"
+        />
+        <TextInput
+          //value={password}
+          label="password"
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Password"
+          placeholderTextColor="#757575"
+        />
+        <TouchableOpacity
+          style={styles.button}>
+          <Text style={{fontSize: 16, fontWeight: 'bold'}}>Sign Up</Text>
+        </TouchableOpacity>
+
+      </View>
+    </View>
     )
 }
 
 export default SignUpScreen
 
-const styles = StyleSheet.create({})
+const {width, height} = Dimensions.get('screen');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: width * 0.04,
+  },
+  input: {
+    width: width * 0.7,
+    height: height * 0.055,
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#bdbdbd',
+    //marginBottom: 10,
+    marginVertical: height * 0.01,
+    borderRadius: width * 0.02,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * 0.7,
+    height: height * 0.055,
+    backgroundColor: '#1de9b6',
+    borderRadius: width * 0.02,
+    marginVertical: height * 0.01,
+  },
+});
