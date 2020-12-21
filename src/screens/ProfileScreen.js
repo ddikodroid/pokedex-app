a0df163f25c332a127686bd753750693b99047a6
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-21 20:00:19 +0700

    wip profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View, Dimensions, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+} from 'react-native';
 import {useSelector} from 'react-redux';
 import axios from 'axios';
 
 const ProfileScreen = ({navigation}) => {
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
   const userData = useSelector((state) => state.data);
-  const [avaPokemon, setAvaPokemon] = useState();
+  const [pokemonAvatar, setPokemonAvatar] = useState();
   useEffect(() => {
     axios
       .get(
@@ -16,35 +20,73 @@ const ProfileScreen = ({navigation}) => {
       )
       .then((response) => {
         console.log(response);
-        setAvaPokemon(response.data.sprites.front_default);
+        setPokemonAvatar(response.data.sprites.front_default);
       });
   });
 
   return (
-    <View style={styles.container}>
+    <>
+      
+    <View style={{alignItems:'center',  backgroundColor:'#fff', flex:1}}><Image source={{uri: pokemonAvatar}} style={styles.image} /></View>
+    <ScrollView style={styles.container}>
+      <>
       <View
         style={{
-          width: width/4,
-          height: height/8,
+          backgroundColor: '#1de9b6',
+          //justifyContent: 'center',
+          paddingTop: 70,
+          alignItems: 'center',
+          width: width,
+          height: height,
+          borderTopLeftRadius: width / 7,
+          borderTopRightRadius: width / 7,
+          marginTop: height * 0.02,
+        }}>
+          
+        <View style={{alignSelf: 'flex-start', paddingHorizontal: width / 10}}>
+          <Text>My name is</Text>
+          <Text style={{fontSize: 32, fontWeight: 'bold'}}>
+            {userData.lastName}
+          </Text>
+          <Text style={{fontSize: 26}}>{userData.firstName}</Text>
+          <Text>My pokemon is</Text>
+          <Text style={{fontSize: 32, fontWeight: 'bold'}}>
+            {userData.favoritePokemon}
+          </Text>
+        </View>
+        
+      </View>
+
+      <View
+        style={{
+          width: width / 4,
+          height: height / 8,
           borderRadius: 50,
           backgroundColor: 'cyan',
           justifyContent: 'center',
           alignItems: 'center',
-
         }}>
-        <Image source={{uri: avaPokemon}} style={styles.image} />
+        
       </View>
 
-      <Text>{avaPokemon}</Text>
+      <Text>{pokemonAvatar}</Text>
       <Text>Profile Screen</Text>
       <Text>
         Full Name: {userData.firstName} {userData.lastName}
       </Text>
       <Text>Username: {userData.username}</Text>
       <Text>Password: {userData.password}</Text>
-      <Text>Favorite Pokemon: {userData.favoritePokemon.charAt(0).toUpperCase() + userData.favoritePokemon.slice(1)}</Text>
-      
-    </View>
+      <Text>
+        Favorite Pokemon:{' '}
+        {userData.favoritePokemon === userData.favoritePokemon.toUpperCase()
+          ? userData.favoritePokemon.charAt(0) +
+            userData.favoritePokemon.slice(1).toLowerCase()
+          : userData.favoritePokemon.charAt(0).toUpperCase() +
+            userData.favoritePokemon.slice(1)}
+      </Text>
+      </>
+    </ScrollView>
+    </>
   );
 };
 
@@ -53,13 +95,16 @@ const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: width * 0.04,
+    position:'absolute',
+    paddingTop:height*0.2
+    //marginTop:height*0.20
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //padding: width * 0.03,
   },
   image: {
-    height: height / 6,
+    height: height / 4,
     width: width / 2,
-    resizeMode: 'contain',
+    //resizeMode: 'contain',
   },
 });
