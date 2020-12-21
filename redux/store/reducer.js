ba00c86256e0cb52914d9f324e4e4314821d9055
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-21 13:23:51 +0700

    initial commit - redux

---
+++ redux/store/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  firstName: 'Diko',
+  lastName: 'Nabil',
+  username: 'user',
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
