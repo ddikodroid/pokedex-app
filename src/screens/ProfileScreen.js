ab5afd6135bb39cccf5825fdd3cbf1d1deca1b7d
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-22 10:07:51 +0700

    change inline styling to stylesheet

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -26,66 +26,36 @@ const ProfileScreen = ({navigation}) => {
 
   return (
     <>
-      
-    <View style={{alignItems:'center',  backgroundColor:'#fff', flex:1}}><Image source={{uri: pokemonAvatar}} style={styles.image} /></View>
-    <ScrollView style={styles.container}>
-      <>
-      <View
-        style={{
-          backgroundColor: '#1de9b6',
-          //justifyContent: 'center',
-          paddingTop: 70,
-          alignItems: 'center',
-          width: width,
-          height: height,
-          borderTopLeftRadius: width / 7,
-          borderTopRightRadius: width / 7,
-          marginTop: height * 0.02,
-        }}>
-          
-        <View style={{alignSelf: 'flex-start', paddingHorizontal: width / 10}}>
-          <Text>My name is</Text>
-          <Text style={{fontSize: 32, fontWeight: 'bold'}}>
-            {userData.lastName}
-          </Text>
-          <Text style={{fontSize: 26}}>{userData.firstName}</Text>
-          <Text>My pokemon is</Text>
-          <Text style={{fontSize: 32, fontWeight: 'bold'}}>
-            {userData.favoritePokemon}
-          </Text>
+      <View style={styles.avaContainer}>
+        <Image source={{uri: pokemonAvatar}} style={styles.image} />
+      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.profileCard}>
+          <View style={styles.profileContainer}>
+            <Text>My name is</Text>
+            <Text style={styles.fontBig}>{userData.lastName}</Text>
+            <Text style={styles.fontSmall}>{userData.firstName}</Text>
+            <Text>My pokemon is</Text>
+            <Text style={styles.fontBig}>{userData.favoritePokemon}</Text>
+          </View>
         </View>
-        
-      </View>
 
-      <View
-        style={{
-          width: width / 4,
-          height: height / 8,
-          borderRadius: 50,
-          backgroundColor: 'cyan',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        
-      </View>
-
-      <Text>{pokemonAvatar}</Text>
-      <Text>Profile Screen</Text>
-      <Text>
-        Full Name: {userData.firstName} {userData.lastName}
-      </Text>
-      <Text>Username: {userData.username}</Text>
-      <Text>Password: {userData.password}</Text>
-      <Text>
-        Favorite Pokemon:{' '}
-        {userData.favoritePokemon === userData.favoritePokemon.toUpperCase()
-          ? userData.favoritePokemon.charAt(0) +
-            userData.favoritePokemon.slice(1).toLowerCase()
-          : userData.favoritePokemon.charAt(0).toUpperCase() +
-            userData.favoritePokemon.slice(1)}
-      </Text>
-      </>
-    </ScrollView>
+        <Text>{pokemonAvatar}</Text>
+        <Text>Profile Screen</Text>
+        <Text>
+          Full Name: {userData.firstName} {userData.lastName}
+        </Text>
+        <Text>Username: {userData.username}</Text>
+        <Text>Password: {userData.password}</Text>
+        <Text>
+          Favorite Pokemon:{' '}
+          {userData.favoritePokemon === userData.favoritePokemon.toUpperCase()
+            ? userData.favoritePokemon.charAt(0) +
+              userData.favoritePokemon.slice(1).toLowerCase()
+            : userData.favoritePokemon.charAt(0).toUpperCase() +
+              userData.favoritePokemon.slice(1)}
+        </Text>
+      </ScrollView>
     </>
   );
 };
@@ -95,8 +65,8 @@ const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    position:'absolute',
-    paddingTop:height*0.2
+    position: 'absolute',
+    paddingTop: height * 0.2,
     //marginTop:height*0.20
     //alignItems: 'center',
     //justifyContent: 'center',
@@ -107,4 +77,28 @@ const styles = StyleSheet.create({
     width: width / 2,
     //resizeMode: 'contain',
   },
+  profileCard: {
+    backgroundColor: '#1de9b6',
+    paddingTop: 70,
+    alignItems: 'center',
+    width: width,
+    height: height,
+    borderTopLeftRadius: width / 7,
+    borderTopRightRadius: width / 7,
+    marginTop: height * 0.02,
+  },
+  profileContainer: {
+    alignSelf: 'flex-start',
+    paddingHorizontal: width / 10,
+  },
+  avaContainer: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  fontBig: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  fontSmall: {fontSize: 26},
 });
